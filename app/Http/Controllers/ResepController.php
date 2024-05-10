fb79dc122b4d2dee89555becc86acd385889f48d
Valent Vasti <valentvasti2114@gmail.com>
2024-05-11 02:27:30 +0700

    feat: resep API

--- app/Http/Controllers/ResepController.php
+++ app/Http/Controllers/ResepController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Resep;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ResepController extends Controller
 {
@@ -12,7 +14,20 @@ class ResepController extends Controller
      */
     public function index()
     {
-        //
+        try{
+            $resep = Resep::with('produk', 'bahan_baku')->get();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Data resep berhasil diambil!',
+                'data' => $resep
+            ], 200);
+        }catch(\Exception $e){
+            return response()->json([
+                'success' => false,
+                'message' => 'Error: ' . $e->getMessage(),
+            ], 500);
+        }
     }
 
     /**
@@ -20,7 +35,49 @@ class ResepController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validationData = Validator::make(
+                [
+                    'produk_id' => $request->produk_id,
+                    'bahan_baku_id' => $request->stok,
+                    'jumlah' => $request->satuan,
+                ],
+                [
+                    'produk_id' => 'required',
+                    'bahan_baku_id' => 'required',
+                    'jumlah' => 'required',
+                ],
+                [
+                    'produk_id.required' => 'Produk wajib dipilih!',
+                    'bahan_baku_id.required' => 'Bahan baku wajib dipilih!',
+                    'jumlah.required' => 'Jumlah wajib diisi!'
+                ]
+            );
+
+            if ($validationData->fails()) {
+                return response()->json([
+                    'success' => false,
+                    'message' => $validationData->errors()
+                ], 400);
+            }
+
+            $resep = Resep::create([
+                'produk_id' => $request->produk_id,
+                'bahan_baku' => $request->bahan_baku,
+                'jumlah' => $request->jumlah,
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Resep berhasil ditambahkan!',
+                'data' => $resep
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error: ' . $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -28,7 +85,29 @@ class ResepController extends Controller
      */
     public function show(int $id)
     {
-        //
+        try{
+            $resep = Resep::with('produk', 'bahan_baku')->find($id);
+
+            if($resep == null){
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Data resep tidak ditemukan!',
+                    'data' => null
+                ], 404);
+            }
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Data resep berhasil diambil!',
+                'data' => $resep
+            ], 200);
+
+        }catch(\Exception $e){
+            return response()->json([
+                'success' => false,
+                'message' => 'Error: ' . $e->getMessage(),
+            ], 500);
+        }
     }
 
     /**
@@ -36,7 +115,59 @@ class ResepController extends Controller
      */
     public function update(Request $request, int $id)
     {
-        //
+        try {
+            $validationData = Validator::make(
+                [
+                    'produk_id' => $request->produk_id,
+                    'bahan_baku_id' => $request->stok,
+                    'jumlah' => $request->satuan,
+                ],
+                [
+                    'produk_id' => 'required',
+                    'bahan_baku_id' => 'required',
+                    'jumlah' => 'required',
+                ],
+                [
+                    'produk_id.required' => 'Produk wajib dipilih!',
+                    'bahan_baku_id.required' => 'Bahan baku wajib dipilih!',
+                    'jumlah.required' => 'Jumlah wajib diisi!'
+                ]
+            );
+
+            if ($validationData->fails()) {
+                return response()->json([
+                    'success' => false,
+                    'message' => $validationData->errors()
+                ], 400);
+            }
+
+            $resep = Resep::with('produk', 'bahan_baku')->find($id);
+
+            if ($resep == null) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Data resep tidak ditemukan',
+                    'data' => null
+                ], 404);
+            }
+
+            $resep->update([
+                'produk_id' => $request->produk_id,
+                'bahan_baku' => $request->bahan_baku,
+                'jumlah' => $request->jumlah,
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Resep berhasil diperbarui!',
+                'data' => $resep
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error: ' . $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -44,6 +175,29 @@ class ResepController extends Controller
      */
     public function destroy(int $id)
     {
-        //
+        try {
+            $resep = Resep::find($id);
+
+            if ($resep == null) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Data resep tidak ditemukan!',
+                    'data' => null
+                ], 404);
+            }
+
+            $resep->delete();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Resep berhasil dihapus!',
+                'data' => $resep
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error: ' . $e->getMessage()
+            ], 500);
+        }
     }
 }
